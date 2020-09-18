f25fbb09728ea18d1624df53e1296fac54049d2f
u101004132 <dzag17188@gmail.com>
2020-09-18 16:37:08 -0400

    insalled lodash, set up dummy data

--- library-server/app.js
+++ library-server/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 const app = express();
 const PORT = 4000 || process.env.PORT;
 app.use(
   "/graphql",
   graphqlHTTP({
     // gql configs
+    schema,
     graphiql: true,
   })
 );
--- library-server/schema/schema.js
+++ library-server/schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require("graphql");
-
+const _ = require("lodash");
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+let languageData = [
+  { name: "Javascript", genre: "web development", id: "1" },
+  { name: "Python", genre: "front and backend", id: "2" },
+  { name: "C", genre: "backend", id: "3" },
+];
 
 const LanguageType = new GraphQLObjectType({
   name: "Language",
@@ -23,9 +30,11 @@ const RootQuery = new GraphQLObjectType({
       },
       resolve(parent, args) {
         // code to get data from db
+        return _.find(languageData, { id: args.id });
       },
     },
   },
 });
 
 module.exports = new GraphQLSchema({ query: RootQuery });
+// mongodb+srv://dmitriy:MongoTest1!@taskmaster.mamfs.mongodb.net/languagelibrary?retryWrites=true&w=majority
