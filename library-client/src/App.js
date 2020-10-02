337a37735d3093e42868941e00eb2f385a721a4a
u101004132 <dzag17188@gmail.com>
2020-10-02 15:18:17 -0400

    apollo provider set up

--- library-client/src/App.js
+++ library-client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import LanguageList from "./components/LanguageList";
+
+// apollo set up
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Dev Library</h1>
-        <LanguageList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Dev Library</h1>
+          <LanguageList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
