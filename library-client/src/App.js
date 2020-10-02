cf613889d9c4aa97099cede814938f4627c09841
u101004132 <dzag17188@gmail.com>
2020-10-02 15:11:14 -0400

    component set up began

--- library-client/src/App.js
+++ library-client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-
+import LanguageList from "./components/LanguageList";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>React App</h1>
+      <div id="main">
+        <h1>Dev Library</h1>
+        <LanguageList />
       </div>
     );
   }
---
+++ library-client/src/components/LanguageList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class LanguageList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="language-list">
+          <li>Languages</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default LanguageList;
